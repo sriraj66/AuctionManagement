cc9c7db394776115904b8bfad3f69b2b2132363e
Venni16 <vennilavanthebest66@gmail.com>
2024-12-24 18:06:46 +0530

    changed the content in boards,match creation

--- templates/crickscore/match creation/js/main.js
+++
@@ -1,9 +0,0 @@
-import { createPlayerForm } from './components/playerForm.js';
-import { setupModal } from './modal.js';
-import { setupTeamsDisplay } from './teamsDisplay.js';
-
-// Initialize the page
-setupModal();
-setupTeamsDisplay();
-
-
